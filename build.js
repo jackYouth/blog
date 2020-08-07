3a3748fb2b9a3fef649af9685aaccc83df20293d
suyd <yandong.su@ifchange.com>
2020-08-07 17:23:06 +0800

    feat: build directory

--- build.js
+++ build.js
@@ -9,32 +9,64 @@ const ROOT_PATH = resolve("./");
 const OUT_PATH = resolve("./");
 const LOOP_DIRS = ["HTML", "JS", "MongoDB", "Nginx", "Other"];
 const FILTER_DIRS = ["imgs"];
+const DEFAULT_TEXT = `# blog \n个人技术博客, 不定时更新, 哈哈哈哈~ \n`;
 
 const isDir = (path) => statSync(path).isDirectory();
 const isMdFile = (file) => extname(file) === ".md";
 
-let text = `# blog \n个人技术博客, 不定时更新, 哈哈哈哈~ \n`;
-
-const creator = (pathname, level) => {
-  const files = readdirSync(pathname);
-  files.forEach((file) => {
+const getDirectoryList = (pathname, level) => {
+  return readdirSync(pathname).reduce((p, file) => {
     const filePath = resolve(pathname, file);
+    let title, children, type;
     if (
       isDir(filePath) &&
       (LOOP_DIRS.includes(file) || level > 0) &&
       !FILTER_DIRS.includes(file)
     ) {
-      text += `\n\n${new Array(level + 2).fill("#").join("")} ${file}`;
-      creator(filePath, level + 1);
+      title = file;
+      children = getDirectoryList(filePath, level + 1);
+      type = "dir";
     } else if (isMdFile(file) && level > 0) {
-      text += `\n- [${basename(file, extname(file))}](.${filePath.replace(
+      title = `[${basename(file, extname(file))}](.${filePath.replace(
         ROOT_PATH,
         ""
       )})`;
+      type = "file";
     }
-  });
+    if (title) {
+      p.push({
+        title,
+        children,
+        type,
+        level,
+      });
+    }
+    return p;
+  }, []);
 };
 
-creator(ROOT_PATH, 0);
+const directoryList = getDirectoryList(ROOT_PATH, 0);
 
-writeFileSync(resolve(OUT_PATH, "README.md"), text);
+const getText = (list, defaultText = "") => {
+  return list.reduce((p, c) => {
+    const { title, children, type, level } = c;
+    if (type === "dir") {
+      if (!level) {
+        p += `\n\n## ${title}`;
+      } else {
+        p += `\n${new Array(level - 1).fill("\t").join("")}- ${title}`;
+      }
+      if (Array.isArray(children) && !!children.length) {
+        p += getText(children);
+      }
+    } else {
+      p += `\n${new Array(level - 1).fill("\t").join("")}- ${title}`;
+    }
+    return p;
+  }, defaultText);
+};
+
+writeFileSync(
+  resolve(OUT_PATH, "README.md"),
+  getText(directoryList, DEFAULT_TEXT)
+);
